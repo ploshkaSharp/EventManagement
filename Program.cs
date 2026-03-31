e5ce6c3b7b689a79e062c590a61bcef71e2a1e8d
ploshkaSharp <aleksey.tutaev@yandex.ru>
2026-03-31 21:25:06 +0400

    feat: Swagger

--- Program.cs
+++ Program.cs
@@ -4,10 +4,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 // singleton ( = in-memory)
 builder.Services.AddSingleton<IEventService, EventService>();
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();    
+}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
