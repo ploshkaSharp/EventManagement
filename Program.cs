5b9ade195fb5035209d9f7e5194760eb9e988fca
ploshkaSharp <aleksey.tutaev@yandex.ru>
2026-05-25 19:42:07 +0400

    feat: DB Scheme create

--- Program.cs
+++ Program.cs
@@ -2,12 +2,28 @@ using System.Reflection;
 using Microsoft.OpenApi;
 using EventManagement.Services;
 using EventManagement.Middleware;
+using EventManagement.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+
+// Configure Database
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.EnableSensitiveDataLogging(builder.Environment.IsDevelopment());
+    options.EnableDetailedErrors(builder.Environment.IsDevelopment());
+});
+
+// Register Services (DbContext is scoped)
+builder.Services.AddScoped<IEventService, EventService>();
+builder.Services.AddScoped<IBookingService, BookingService>();
+// Register Background Service (singleton)
+builder.Services.AddHostedService<BookingBackgroundService>();
 
 // Настройка Swagger с поддержкой XML-комментариев
 builder.Services.AddSwaggerGen(c =>
@@ -27,12 +43,16 @@ builder.Services.AddSwaggerGen(c =>
     // Добавление аннотаций для типов ответов
     c.EnableAnnotations();
 });
-// Singleton (in-memory)
-builder.Services.AddSingleton<IEventService, EventService>();
-builder.Services.AddSingleton<IBookingService, BookingService>();
-builder.Services.AddHostedService<BookingBackgroundService>();
 
 var app = builder.Build();
+
+// Ensure database is created
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
 // middleware для глобальной обработки ошибок. Ставить первым в pipeline для перехвата всех исключений
 app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
 
